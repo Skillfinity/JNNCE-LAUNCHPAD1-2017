e5cf029cb91b92e6b45b8effa27423190ed6cd22
Kubsad1895 <kubsad1895@gmail.com>
2017-03-22 19:46:18 +0530

    assignment prime
    assignment prime

---
+++ prime/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+ int i, j, n, isPrime;
+
+
+        printf("Find prime numbers between 0 to : ");
+        scanf("%d", &n);
+        printf("All prime numbers between 0 to %d are:\n", n);
+
+        for(i=2; i<=n; i++)
+        {
+            isPrime=1;
+            for(j=2; j<=i/2; j++)
+            {
+                if(i%j==0)
+                {
+                   isPrime = 0;
+                    break;
+                }
+            }
+
+        if(isPrime==1)
+            {
+                printf("%d, ", i);
+            }
+        }
+
+return 0;
+}
